e2507b0088eb56b6893ae963c6f35ccc24ff5dc1
김진우 <jinwoo3661@naver.com>
2025-03-23 20:07:14 +0900

    test: ci/cd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,6 @@ android {
 
         buildConfigField("String", "KAKAO_NATIVE_APP_KEY", properties["kakao.native.app.key"].toString())
 
-        manifestPlaceholders["IO_SENTRY_DSN"] = properties["io.sentry.dsn"] as String
         manifestPlaceholders["KAKAO_NATIVE_APP_KEY_MANIFEST"] = properties["kakao.native.app.key.manifest"] as String
     }
 
