a721c0a80e3cdcab84647ecfa913baeb665f8dd0
김진우 <85734140+jinuemong@users.noreply.github.com>
2025-03-29 12:03:49 +0900

    [feat] 코스 등록 네비게이션 구현 & 디자인 요구사항 적용  (#18)
    * feat: 포인트 부족 시 코스 등록 연결
    
    * feat: 코스 상세에서 포인트 부족 시 코스 등록 연결
    
    * feat: 중복 로직 제거
    
    * refactor : 포인트 수집 ui 변경사항 적용
    
    * style: 코드 포맷 수정 & 린트 체크

--- app/src/main/java/org/sopt/teamdateroad/presentation/ui/coursedetail/CourseDetailScreen.kt
+++ app/src/main/java/org/sopt/teamdateroad/presentation/ui/coursedetail/CourseDetailScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,23 +26,16 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.flowWithLifecycle
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.PagerState
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.rewarded.RewardedAd
-import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
-import org.sopt.teamdateroad.BuildConfig
 import org.sopt.teamdateroad.R
 import org.sopt.teamdateroad.domain.model.CourseDetail
 import org.sopt.teamdateroad.domain.model.Place
 import org.sopt.teamdateroad.presentation.type.CourseDetailUnopenedDetailType
 import org.sopt.teamdateroad.presentation.type.DateTagType.Companion.getDateTagTypeByName
 import org.sopt.teamdateroad.presentation.type.EnrollType
-import org.sopt.teamdateroad.presentation.type.OneButtonDialogWithDescriptionType
 import org.sopt.teamdateroad.presentation.type.TwoButtonDialogWithDescriptionType
 import org.sopt.teamdateroad.presentation.ui.component.bottomsheet.DateRoadBasicBottomSheet
 import org.sopt.teamdateroad.presentation.ui.component.bottomsheet.DateRoadPointBottomSheet
 import org.sopt.teamdateroad.presentation.ui.component.bottomsheet.model.collect.DateRoadCollectPointType
-import org.sopt.teamdateroad.presentation.ui.component.dialog.DateRoadOneButtonDialogWithDescription
 import org.sopt.teamdateroad.presentation.ui.component.dialog.DateRoadTwoButtonDialogWithDescription
 import org.sopt.teamdateroad.presentation.ui.component.pager.DateRoadImagePager
 import org.sopt.teamdateroad.presentation.ui.component.topbar.DateRoadScrollResponsiveTopBar
@@ -55,7 +47,6 @@ import org.sopt.teamdateroad.presentation.ui.coursedetail.component.CourseDetail
 import org.sopt.teamdateroad.presentation.ui.coursedetail.component.CourseDetailBottomBar
 import org.sopt.teamdateroad.presentation.ui.coursedetail.component.CourseDetailUnopenedDetail
 import org.sopt.teamdateroad.presentation.ui.coursedetail.component.courseDetailOpenedDetail
-import org.sopt.teamdateroad.presentation.util.AdsAmplitude
 import org.sopt.teamdateroad.presentation.util.CourseDetail.POINT_LACK
 import org.sopt.teamdateroad.presentation.util.CourseDetailAmplitude.CLICK_COURSE_BACK
 import org.sopt.teamdateroad.presentation.util.CourseDetailAmplitude.CLICK_COURSE_PURCHASE
@@ -79,8 +70,6 @@ fun CourseDetailRoute(
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val lifecycleOwner = LocalLifecycleOwner.current
-    val context = LocalContext.current
-    val adRequest = remember { AdRequest.Builder().build() }
 
     LaunchedEffect(viewModel.sideEffect, lifecycleOwner) {
         viewModel.sideEffect.flowWithLifecycle(lifecycle = lifecycleOwner.lifecycle)
@@ -88,25 +77,6 @@ fun CourseDetailRoute(
                 when (courseDetailSideEffect) {
                     is CourseDetailContract.CourseDetailSideEffect.NavigateToEnroll -> navigateToEnroll(courseDetailSideEffect.enrollType, courseDetailSideEffect.viewPath, courseDetailSideEffect.id)
                     is CourseDetailContract.CourseDetailSideEffect.PopBackStack -> popBackStack()
-                    CourseDetailContract.CourseDetailSideEffect.NavigateToAds -> {
-                        RewardedAd.load(
-                            context,
-                            BuildConfig.GOOGLE_ADS_API_ID,
-                            adRequest,
-                            object : RewardedAdLoadCallback() {
-                                override fun onAdLoaded(ad: RewardedAd) {
-                                    viewModel.postAdsPoint()
-                                }
-
-                                override fun onAdFailedToLoad(error: LoadAdError) {
-                                    when (error.code) {
-                                        AdRequest.ERROR_CODE_NO_FILL -> viewModel.setEvent(CourseDetailContract.CourseDetailEvent.FullAds)
-                                        else -> viewModel.setEvent(CourseDetailContract.CourseDetailEvent.FailLoadAdsPoint)
-                                    }
-                                }
-                            }
-                        )
-                    }
                 }
             }
     }
@@ -175,22 +145,11 @@ fun CourseDetailRoute(
                     viewModel.setEvent(CourseDetailContract.CourseDetailEvent.OnReportWebViewClicked)
                 },
                 onReportWebViewClose = { viewModel.setEvent(CourseDetailContract.CourseDetailEvent.DismissReportWebView) },
-                onDismissCollectPoint = {
-                    AmplitudeUtils.trackEvent(eventName = AdsAmplitude.CLICK_COLLECT_POINT_CLOSE)
-                    viewModel.setEvent(CourseDetailContract.CourseDetailEvent.DismissDialogPointLack)
-                },
                 onSelectEnroll = {
-                    AmplitudeUtils.trackEvent(eventName = AdsAmplitude.CLICK_COURSE)
-                    viewModel.setEvent(CourseDetailContract.CourseDetailEvent.DismissDialogPointLack)
                     viewModel.setSideEffect(CourseDetailContract.CourseDetailSideEffect.NavigateToEnroll(enrollType = EnrollType.COURSE, viewPath = COURSE_DETAIL, id = null))
                 },
-                onSelectAds = {
-                    AmplitudeUtils.trackEvent(eventName = AdsAmplitude.CLICK_AD)
+                onDismissCollectPoint = {
                     viewModel.setEvent(CourseDetailContract.CourseDetailEvent.DismissDialogPointLack)
-                    viewModel.setSideEffect(CourseDetailContract.CourseDetailSideEffect.NavigateToAds)
-                },
-                onDismissFullAdsDialog = {
-                    viewModel.setEvent(CourseDetailContract.CourseDetailEvent.DismissFullAdsDialog)
                 }
             )
         }
@@ -242,9 +201,7 @@ fun CourseDetailScreen(
     dismissReportCourseBottomSheet: () -> Unit,
     enrollSchedule: () -> Unit,
     onTopBarIconClicked: () -> Unit,
-    openCourseDetail: () -> Unit,
-    onSelectAds: () -> Unit,
-    onDismissFullAdsDialog: () -> Unit
+    openCourseDetail: () -> Unit
 ) {
     var imageHeight by remember { mutableIntStateOf(0) }
 
@@ -393,24 +350,20 @@ fun CourseDetailScreen(
                 )
             }
 
-            if (courseDetailUiState.isFullAdsDialogOpen) {
-                DateRoadOneButtonDialogWithDescription(
-                    oneButtonDialogWithDescriptionType = OneButtonDialogWithDescriptionType.FULL_ADS,
-                    onDismissRequest = onDismissFullAdsDialog,
-                    onClickConfirm = onDismissFullAdsDialog
-                )
-            }
-
             DateRoadPointBottomSheet(
                 isBottomSheetOpen = courseDetailUiState.isPointCollectBottomSheetOpen,
                 title = stringResource(R.string.point_box_lack_point_button_text),
                 onClick = { dateRoadCollectPointType ->
                     when (dateRoadCollectPointType) {
-                        DateRoadCollectPointType.WATCH_ADS -> onSelectAds()
+                        // TODO  : add ADS
+                        DateRoadCollectPointType.WATCH_ADS -> Unit
                         DateRoadCollectPointType.COURSE_REGISTRATION -> onSelectEnroll()
                     }
+                    onDismissCollectPoint()
                 },
-                onDismissRequest = onDismissCollectPoint
+                onDismissRequest = {
+                    onDismissCollectPoint()
+                }
             )
 
             DateRoadBasicBottomSheet(
@@ -469,12 +422,10 @@ fun CourseDetailScreenPreview() {
                 places = listOf(
                     Place(
                         title = "Place 1",
-                        address = "Address 1",
                         duration = "1"
                     ),
                     Place(
                         title = "Place 2",
-                        address = "Address 2",
                         duration = "2"
                     )
                 ),
@@ -510,9 +461,7 @@ fun CourseDetailScreenPreview() {
             dismissDialogDeleteCourse = {},
             onDismissCollectPoint = {},
             onSelectEnroll = {},
-            dismissDialogReportCourse = {},
-            onSelectAds = {},
-            onDismissFullAdsDialog = {}
+            dismissDialogReportCourse = {}
         )
     }
 }
