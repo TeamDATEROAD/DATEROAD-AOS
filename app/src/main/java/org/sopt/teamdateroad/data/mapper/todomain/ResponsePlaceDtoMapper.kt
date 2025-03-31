77e609e3d6a6eb3d0f9117109e9098a37dcc1e98
dpcks0509 <dpcks0509@gmail.com>
2025-03-31 13:15:38 +0900

    refactor: address nullabel 제거

--- app/src/main/java/org/sopt/teamdateroad/data/mapper/todomain/ResponsePlaceDtoMapper.kt
+++ app/src/main/java/org/sopt/teamdateroad/data/mapper/todomain/ResponsePlaceDtoMapper.kt
@@ -6,6 +6,6 @@ import org.sopt.teamdateroad.domain.model.Place
 
 fun ResponsePlaceDto.toDomain() = Place(
     title = this.title,
-    address = this.address ?: "서울 광진구 자양동 123-456",
+    address = this.address,
     duration = this.duration.toDuration()
 )
