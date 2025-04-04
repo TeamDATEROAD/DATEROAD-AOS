7274457e1d18715c35d4ec0606ef867aa0fe04f7
dpcks0509 <dpcks0509@gmail.com>
2025-04-04 09:48:01 +0900

    remove: PlaceSearchUiState 제거

--- app/src/main/java/org/sopt/teamdateroad/presentation/ui/enroll/placesearch/PlaceSearchUiState.kt
+++
@@ -1,9 +0,0 @@
-package org.sopt.teamdateroad.presentation.ui.enroll.placesearch
-
-import androidx.paging.compose.LazyPagingItems
-import org.sopt.teamdateroad.domain.model.PlaceInfo
-
-data class PlaceSearchUiState(
-    val searchKeyword: String,
-    val searchPlaceInfos: LazyPagingItems<PlaceInfo>
-)
